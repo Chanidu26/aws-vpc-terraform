0b29493ce10cdeaccae23b1d6ee8c0726938b071
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-03-16 15:13:14 +0530

    added aws dynamodb state lock

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket         = "chanidu-terraform-state-bucket"                     
     key            = "vpc_terraform_state_dir/terraform.tfstate"      
     region         = "us-east-1"
+    encrypt        =  true
+    dynamodb_table = "chanidu-terraform-state-lock"
   }
 }
